bf13f86d850f254d6cbecaee7ab33a51f51b1e41
Igor Lupats <curdcurator@gmail.com>
2026-07-24 20:53:19 +0500

    it will get better anytime soon

--- main/Main.java
+++ main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import main.io.IOReader;
+import main.crypto.EnDeCrypt;
 import java.util.ArrayList;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,16 +9,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        try (FileInputStream fis = new FileInputStream("/storage/emulated/0/Download/voynaimir.txt"))
+        EnDeCrypt crypto = new EnDeCrypt();
 
-        {
-            IOReader iReader = new IOReader();
-
-            System.out.println(iReader.readPortion(fis));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        crypto.action();
     }
 
 }
